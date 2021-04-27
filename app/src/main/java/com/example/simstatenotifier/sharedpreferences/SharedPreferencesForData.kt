5881b141a4a52e9bf2d565b996d17d311469c736
Priyanka Badgujar <priyankabadgujar97@gmail.com>
2021-04-27 23:42:22 +0530

    Unused code remove

--- app/src/main/java/com/example/simstatenotifier/sharedpreferences/SharedPreferencesForData.kt
+++ app/src/main/java/com/example/simstatenotifier/sharedpreferences/SharedPreferencesForData.kt
@@ -14,18 +14,6 @@ class SharedPreferencesForData(context: Context) {
         ApplicationConstants.SHARED_PREF_SIM_DATA,
         MODE_PRIVATE)
 
-//    fun saveString(KEY_NAME: String, text: String) {
-//        val editor: SharedPreferences.Editor = sharedPref.edit()
-//        editor.putString(KEY_NAME, text)
-//        editor.apply()
-//    }
-//
-//    fun saveInt(KEY_NAME: String, value: Int) {
-//        val editor: SharedPreferences.Editor = sharedPref.edit()
-//        editor.putInt(KEY_NAME, value)
-//        editor.apply()
-//    }
-
     fun saveHashMap(KEY_SLOT_NO: String, displayName: String, subscriptionId: String, mobileNo: String) {
         val simDataMap = HashMap<String, String>()
         simDataMap[ApplicationConstants.KEY_DISPLAY_NAME] = displayName
